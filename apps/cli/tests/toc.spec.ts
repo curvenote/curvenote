bb76f4b920a722ba89049b53facc35473f64de1b
stevejpurves <steve@curvenote.com>
2022-09-13 22:12:08 +0100

    remove misleading test case

--- apps/cli/tests/toc.spec.ts
+++ apps/cli/tests/toc.spec.ts
@@ -141,32 +141,4 @@ describe('Writing JB TOC', () => {
     expect(hasParts).toEqual(false);
     expect(skipCounter).toEqual(0);
   });
-  test('ignore missing/orphaned parentIds in a group', () => {
-    const { items, hasParts, skipCounter, totalDocuments } = unflattenNavBlocks([
-      {
-        id: '1',
-        kind: NavListItemKindEnum.Group,
-        title: 'one',
-      },
-      {
-        id: '2',
-        kind: NavListItemKindEnum.Item,
-        parentId: '1',
-        block: { data: { hidden: false } } as Block,
-      },
-      {
-        id: '3',
-        kind: NavListItemKindEnum.Item,
-        parentId: '99',
-        block: { data: { hidden: false } } as Block,
-      },
-    ]);
-    expect(items).toHaveLength(2);
-    expect(items[0].children).toHaveLength(1);
-    expect(items[0].children[0].id).toEqual('2');
-    expect(items[1].id).toEqual('3');
-    expect(totalDocuments).toEqual(2);
-    expect(hasParts).toEqual(true);
-    expect(skipCounter).toEqual(0);
-  });
 });
