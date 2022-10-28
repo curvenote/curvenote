5a1b2f182092233240d8fe0632287be6f7d198d1
Rowan Cockett <rowanc1@gmail.com>
2022-10-28 15:53:54 -0600

    🚀 Myst merge

--- packages/curvenote/jest.config.cjs
+++ packages/curvenote/jest.config.cjs
@@ -1,15 +1,14 @@
 module.exports = {
   rootDir: '../../',
   preset: 'ts-jest/presets/js-with-ts',
-  testMatch: ['<rootDir>/apps/cli/**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testMatch: ['<rootDir>/packages/curvenote/**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   testTimeout: 10000,
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|xml)$':
-      '<rootDir>/apps/cli/tests/__mocks__/fileMock.js',
-    mystjs: '<rootDir>/node_modules/mystjs/dist/index.umd.js',
+      '<rootDir>/packages/curvenote/tests/__mocks__/fileMock.js',
     '#(.*)': '<rootDir>/node_modules/$1', // https://github.com/chalk/chalk/issues/532
   },
   globals: {
@@ -20,7 +19,7 @@ module.exports = {
   verbose: true,
   testEnvironment: 'node',
   transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!(vfile|vfile-message|unified|bail|trough|zwitch|unist-|hast-|rehype-|mdast-|trim-|web-namespaces|fetch-blob|formdata-polyfill|property-information|space-separated-tokens|comma-separated-tokens|get-port))',
+    '<rootDir>/node_modules/(?!(vfile|vfile-message|unified|bail|trough|zwitch|unist-|hast-|html-|rehype-|mdast-|micromark-|trim-|web-namespaces|fetch-blob|formdata-polyfill|property-information|space-separated-tokens|comma-separated-tokens|get-port|data-uri-to-buffer|stringify-entities|character-entities-html4|ccount))',
   ],
   testPathIgnorePatterns: ['/node_modules/', '/.yalc/', '/dist/'],
 };
