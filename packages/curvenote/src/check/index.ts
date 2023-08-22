91281996d798c88b3616e3a8f9bde0d7ae40cdf8
Franklin Koch <franklinwkoch@gmail.com>
2023-08-22 00:15:33 -0600

    ✅ Add functions to run and log checks

--- packages/curvenote/src/check/index.ts
+++ packages/curvenote/src/check/index.ts
@@ -1,2 +1,3 @@
+export * from './runner.js';
 export * from './types.js';
 export * from './validators.js';
---
+++ packages/curvenote/src/check/runner.ts
@@ -0,0 +1,90 @@
+import { incrementOptions } from 'simple-validators';
+import type { ISession } from '../session/types.js';
+import { CheckStatus } from './types.js';
+import type { Check, CheckDefinition, CheckInterface, CheckReport, CheckResult } from './types.js';
+import { validateCheck } from './validators.js';
+import chalk from 'chalk';
+
+export async function runChecks(
+  session: ISession,
+  file: string,
+  checks: Check[],
+  checkDefinitions: CheckInterface[],
+): Promise<CheckReport> {
+  const opts = { property: 'checks', messages: {} };
+  const completedChecks = (
+    await Promise.all(
+      checks.map(async (check, index) => {
+        const validCheck = validateCheck(
+          session,
+          check,
+          checkDefinitions,
+          incrementOptions(`${index}`, opts),
+        );
+        if (!validCheck) return undefined;
+        const checkDefinition = checkDefinitions.find((def) => check.id === def.id);
+        const result = await checkDefinition?.validate(session, file, validCheck);
+        return result ? { ...checkDefinition, ...result } : undefined;
+      }),
+    )
+  ).filter((check): check is CheckDefinition & CheckResult => !!check);
+  let finalStatus = CheckStatus.pass;
+  const checkCategories: Record<string, CheckReport['results'][0]> = {};
+  completedChecks.forEach((check) => {
+    const { category, status } = check;
+    if (!checkCategories[category]) {
+      checkCategories[category] = {
+        category,
+        status,
+        checks: [check],
+      };
+    } else {
+      checkCategories[category].checks.push(check);
+    }
+    if (status !== CheckStatus.pass) {
+      checkCategories[category].status = CheckStatus.fail;
+      finalStatus = CheckStatus.fail;
+    }
+  });
+  return { status: finalStatus, results: Object.values(checkCategories) };
+}
+
+export function logCheckReport(session: ISession, report: CheckReport) {
+  const checkFail = (msg: string, prefix?: string) => chalk.red(`${prefix ?? ''}❌ ${msg}`);
+  const checkError = (msg: string, prefix?: string) => chalk.yellow(`${prefix ?? ''}⚠️  ${msg}`);
+  const checkPass = (msg: string, prefix?: string) => chalk.green(`${prefix ?? ''}✅ ${msg}`);
+  if (report.status === CheckStatus.pass) {
+    session.log.info(checkPass('All checks passed!'));
+  } else {
+    session.log.error(checkFail('Checks did not all pass'));
+  }
+  report.results.forEach((result) => {
+    const { status, category, checks } = result;
+    if (status === CheckStatus.pass) {
+      session.log.info(
+        checkPass(`${category} (${checks.length}/${checks.length} tests passed)`, '  '),
+      );
+    } else {
+      session.log.error(
+        checkFail(
+          `${category} (${checks.filter((c) => c.status === CheckStatus.pass).length}/${
+            checks.length
+          } tests passed)`,
+          '  ',
+        ),
+      );
+    }
+    checks.forEach((check) => {
+      const { message } = check;
+      if (status === CheckStatus.pass) {
+        session.log.debug(checkPass(message, '    '));
+      } else if (check.status === CheckStatus.pass) {
+        session.log.info(checkPass(message, '    '));
+      } else if (check.status === CheckStatus.error) {
+        session.log.error(checkError(message, '    '));
+      } else {
+        session.log.error(checkFail(message, '    '));
+      }
+    });
+  });
+}
