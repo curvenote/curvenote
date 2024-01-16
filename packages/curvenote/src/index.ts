593b0bde378d4c1504ef93186e962815a7000cef
stevejpurves <steve@curvenote.com>
2024-01-16 17:45:22 +0000

    🔌 expose submissions interface in ts/js

--- packages/curvenote/src/index.ts
+++ packages/curvenote/src/index.ts
@@ -6,4 +6,5 @@ export * as sync from './sync/index.js';
 export * as check from './check/index.js';
 export * as works from './works/index.js';
 export * as utils from './utils/index.js';
+export * as submissions from './submissions/index.js';
 export { default as version } from './version.js';
--- packages/curvenote/src/submissions/index.ts
+++ packages/curvenote/src/submissions/index.ts
@@ -1,2 +1,3 @@
 export { submit } from './submit.js';
 export { list } from './list.js';
+export { postNewWork, postNewSubmission } from './utils.js';
