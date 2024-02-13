eb4b3f169dee42bb3529e7668f67840412c8c441
Franklin Koch <franklinwkoch@gmail.com>
2024-02-13 12:22:47 -0700

    🪵 Log check results to file

--- packages/curvenote/src/cli/check.ts
+++ packages/curvenote/src/cli/check.ts
@@ -1,23 +1,38 @@
-import { Command } from 'commander';
-import { logCheckReport, runChecks } from '../check/runner.js';
+import { Command, Option } from 'commander';
+import { logCheckReport, runChecks, writeCheckReport } from '../check/runner.js';
 import type { ISession } from '../session/types.js';
 import { clirun } from './utils.js';
 import { submissionRuleChecks } from '@curvenote/check-implementations';
 import { build } from 'myst-cli';
 
-async function exampleChecks(session: ISession) {
+async function exampleChecks(session: ISession, opts?: { log?: boolean; logfile?: string }) {
   await build(session, [], { all: true, checkLinks: true });
-  const checks = [...submissionRuleChecks, ...(session.plugins?.checks ?? [])];
+  const checks = session.plugins?.checks?.length ? session.plugins.checks : submissionRuleChecks;
   const checkIds = checks.map(({ id }) => {
     return { id };
   });
   const report = await runChecks(session, checkIds, checks);
+  if (opts?.log) {
+    writeCheckReport(session, report, opts?.logfile);
+  }
   logCheckReport(session, report);
+}
+export function makeLogfileOption() {
+  return new Option(
+    '--logfile <name>',
+    'File to write check result logs; by default, a file will be written to the _build/temp directory',
+  );
+}
+
+export function makeLogOption() {
+  return new Option('--log', 'Write check results to file').default(true);
 }
 
 export function makeCheckCLI(program: Command) {
   const command = new Command('check')
-    .description('Run some example checks on your MyST project')
+    .description('Run checks from plugins or example checks on your MyST project')
+    .addOption(makeLogOption())
+    .addOption(makeLogfileOption())
     .action(clirun(exampleChecks, { program }));
   return command;
 }
