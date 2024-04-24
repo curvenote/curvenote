93b9f12ee6c7126d942ebab646087ffc11e0f6e7
stevejpurves <steve@curvenote.com>
2024-04-24 21:20:55 +0100

    🪜rebase and feedback

--- packages/curvenote/.eslintrc.cjs
+++ packages/curvenote/.eslintrc.cjs
@@ -1,10 +1,4 @@
 module.exports = {
   root: true,
   extends: ['curvenote'],
-  rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      { devDependencies: true, optionalDependencies: false, peerDependencies: true },
-    ],
-  },
 };
