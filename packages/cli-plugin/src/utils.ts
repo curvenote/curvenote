58999f50d8030cfa4b4e40d9aaf4b4598a7f90cd
stevejpurves <steve@curvenote.com>
2024-05-15 16:21:36 +0100

    🪪 default content

--- packages/cli-plugin/src/utils.ts
+++ packages/cli-plugin/src/utils.ts
@@ -1,4 +1,6 @@
 import type { VFile } from 'vfile';
+import type { DirectiveData } from 'myst-common';
+import { u } from 'unist-builder';
 
 export function validateStringOptions(
   vfile: VFile,
@@ -12,4 +14,31 @@ export function validateStringOptions(
   ) {
     vfile.message(`Invalid ${fieldName} supplied must be one of (${validValues.join(' | ')}).`);
   }
+}
+
+export function makePlaceholder(data: DirectiveData, description: string) {
+  const optionList = data.options
+    ? [
+        u(
+          'ul',
+          Object.entries(data.options)?.map(([key, value]) =>
+            u('listItem', [u('inlineCode', key), u('text', `: ${String(value)}`)]),
+          ) ?? [],
+        ),
+      ]
+    : [];
+
+  return [
+    u('admonition', { kind: 'important' }, [
+      u('admonitionTitle', [u('inlineCode', [u('text', data.name)])]),
+      u('paragraph', [
+        u('text', 'This block will be replaced with '),
+        u('strong', [u('text', description)]),
+        u('text', ' when deployed to '),
+        u('link', { url: 'https://curvenote.com' }, [u('text', 'Curvenote')]),
+        u('text', '.'),
+      ]),
+      u('paragraph', [u('text', 'Options:'), ...optionList]),
+    ]),
+  ];
 }
