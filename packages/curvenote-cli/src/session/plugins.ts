1052bffc52994ebee8640fd2157f5c16454d5440
Franklin Koch <franklinwkoch@gmail.com>
2025-06-23 22:02:57 -0600

    🔧 Consume plugin loading changes

--- packages/curvenote-cli/src/session/plugins.ts
+++ packages/curvenote-cli/src/session/plugins.ts
@@ -1,26 +1,35 @@
 import fs from 'node:fs';
 import { plural } from 'myst-common';
-import { loadPlugins, selectors } from 'myst-cli';
+import { loadPlugins } from 'myst-cli';
 import type { CurvenotePlugin, ISession, ValidatedCurvenotePlugin } from './types.js';
+import type { PluginInfo } from 'myst-config';
 
 /**
  * Load user-defined plugin modules declared in the project frontmatter
  *
  * @param session session with logging
  */
-export async function loadProjectPlugins(session: ISession): Promise<ValidatedCurvenotePlugin> {
-  const config = selectors.selectCurrentProjectConfig(session.store.getState());
-  const mystPlugins = await loadPlugins(session);
-  const plugins: ValidatedCurvenotePlugin = {
-    ...mystPlugins,
+export async function loadProjectPlugins(
+  session: ISession,
+  plugins: PluginInfo[],
+): Promise<ValidatedCurvenotePlugin> {
+  const loadedPlugins: ValidatedCurvenotePlugin = {
     checks: [],
+    checksPaths: [],
+    ...(await loadPlugins(session, plugins)),
   };
-  if (!config?.plugins || config.plugins.length === 0) {
-    return plugins;
+
+  // Deduplicate by checksPath (must be separate than path, which is already written at this point)...
+  const newChecksPlugins = [...new Map(plugins.map((info) => [info.path, info])).values()].filter(
+    // ...and filter out already loaded plugins
+    ({ path }) => !loadedPlugins.checksPaths.includes(path),
+  );
+  if (newChecksPlugins.length === 0) {
+    return loadedPlugins;
   }
   // Already validated by myst plugin loader
   const modules = await Promise.all(
-    config?.plugins?.map(async (plugin) => {
+    newChecksPlugins.map(async (plugin) => {
       if (fs.statSync(plugin.path).isFile() && plugin.path.endsWith('.mjs')) {
         let module: any;
         try {
@@ -44,9 +53,9 @@ export async function loadProjectPlugins(session: ISession): Promise<ValidatedCu
         }) also loaded loaded: ${plural('%s check(s)', checks)}`,
       );
       // TODO: validate each check
-      plugins.checks.push(...checks);
+      loadedPlugins.checks.push(...checks);
     }
   });
   session.log.debug('Project Plugins loaded');
-  return plugins;
+  return loadedPlugins;
 }
