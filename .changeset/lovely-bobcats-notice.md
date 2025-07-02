0de0f5b794e26702264844a0517f568af1026ebe
Franklin Koch <franklinwkoch@gmail.com>
2025-07-02 14:57:39 -0600

    🌎 Allow works to be pushed to public CDN

---
+++ .changeset/lovely-bobcats-notice.md
@@ -0,0 +1,6 @@
+---
+"@curvenote/cli": patch
+"curvenote": patch
+---
+
+Allow works to be pushed to public CDN
--- packages/curvenote-cli/src/works/types.ts
+++ packages/curvenote-cli/src/works/types.ts
@@ -1,4 +1,8 @@
-import type { BaseLog, IdAndDate } from '../logs/types.js';
+import type { BaseLog, IdAndDate, BaseOpts } from '../logs/types.js';
+
+export type PushOpts = BaseOpts & {
+  public?: boolean;
+};
 
 export type WorkPushLog = BaseLog & {
   work?: IdAndDate;
