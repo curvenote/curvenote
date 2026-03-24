52bc4b851c52f7205799296a8b86db6fae7d3eb0
Steve Purves <steve@curvenote.com>
2026-03-24 22:13:44 +0000

    fix icon

--- packages/scms-core/src/components/navigation/MenuIcon.tsx
+++ packages/scms-core/src/components/navigation/MenuIcon.tsx
@@ -176,7 +176,7 @@ export function MenuIcon({
       return <LayoutDashboard className={classNameWithDefaults} />;
     case 'square-library':
       return <SquareLibrary className={classNameWithDefaults} />;
-    case 'admin.jobs':
+    case 'system.jobs':
     case 'square-play':
       return <SquarePlay className={classNameWithDefaults} />;
     case 'share':
