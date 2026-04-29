d1ee1f8534880e0ca1c11eeec08c24de676a991b
Steve Purves <steve@curvenote.com>
2026-04-29 18:11:05 +0100

    tweak messages

--- platform/scms/app/routes/app/works.$workId.upload.$workVersionId/CaptureMetadataSection.tsx
+++ platform/scms/app/routes/app/works.$workId.upload.$workVersionId/CaptureMetadataSection.tsx
@@ -23,7 +23,7 @@ export function CaptureMetadataSection({ title, authors }: CaptureMetadataSectio
       className="space-y-4 max-w-3xl"
     >
       <p className="text-muted-foreground">
-        Using a meaningful title, will help you find this paper again later, please add one now.
+        Using a meaningful title will help you find this paper again later, please add one.
       </p>
       <ui.Card className="px-6 pt-4 pb-6 space-y-4">
         <WorkTitleForm title={title} />
