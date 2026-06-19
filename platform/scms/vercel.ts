d0d1a6b8b4240a4fc2e0fcb8864cf8bd14dd6257
Steve Purves <steve@curvenote.com>
2026-06-19 14:39:14 +0100

    🔥🔧 remove stray vercel q config

--- platform/scms/vercel.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelConfig } from '@vercel/config/v1';
-
-export const config: VercelConfig = {
-  functions: {
-    'app/routes/api/v1.jobs.push-to-drain/route.tsx': {
-      maxDuration: 300,
-    },
-  },
-};
