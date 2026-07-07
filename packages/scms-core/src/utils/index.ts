f4168547623bfd4d5b89d1340a2c75a143444745
Steve Purves <steve@curvenote.com>
2026-07-07 17:52:41 +0100

    ♻️ Hoist manuscript format helpers to scms-core
    Move manuscriptFormats and isPreviewCandidate into scms-core so both
    the upload route and shared metadata utilities can import the same
    predicates. Route modules become thin re-exports; tests move with them.

--- packages/scms-core/src/utils/index.ts
+++ packages/scms-core/src/utils/index.ts
@@ -22,6 +22,7 @@ export * from './wildcard.js';
 export * from './formatZodError.js';
 export * from './versionTimelineUrls.js';
 export * from './versionTimelineTrim.js';
+export * from './manuscriptFormats.js';
 export * from './workVersionMetadata.js';
 
 export const version = 'v1';
