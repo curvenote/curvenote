2d58dc37cb3e54b02b0ed50bc471f198de792d24
Steve Purves <steve@curvenote.com>
2026-07-10 10:51:27 +0100

    🐛 Fix VersionTagBadge import path in resume dialog

--- packages/scms-core/src/components/ui/dialogs/ResumeDraftWorkDialog.tsx
+++ packages/scms-core/src/components/ui/dialogs/ResumeDraftWorkDialog.tsx
@@ -15,7 +15,7 @@ import { formatDistanceToNow } from 'date-fns';
 import { GitBranch, Trash2 } from 'lucide-react';
 import { plural } from '../../../utils/plural.js';
 import type { DraftWork } from './types.js';
-import { VersionTagBadge } from '../../VersionTagBadge.js';
+import { VersionTagBadge } from '../VersionTagBadge.js';
 
 /**
  * Props for the ResumeDraftWorkDialog component
