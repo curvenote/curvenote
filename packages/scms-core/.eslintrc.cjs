dcd00cd79a4fde00d10a8900868e019633dbc0d6
Steve Purves <steve@curvenote.com>
2026-07-27 09:31:03 +0100

    🔧 Ignore .d.ts files in scms-core eslint

--- packages/scms-core/.eslintrc.cjs
+++ packages/scms-core/.eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   extends: ['curvenote'],
+  ignorePatterns: ['**/*.d.ts'],
   rules: {
     // Allow React.forwardRef(function ComponentName ...) which shadows the outer const.
     '@typescript-eslint/no-shadow': 'off',
