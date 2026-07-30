dd4e10b5db43966cd320fac6805c350326298dd2
Steve Purves <steve@curvenote.com>
2026-07-30 14:33:47 +0100

    🔌 Enable Open Science extension

---
+++ platform/scms/app/extensions/client.ts
@@ -0,0 +1,6 @@
+import { extension as curvenoteScmsSitesExtClient } from '@curvenote/scms-sites-ext/client';
+import { extension as hhmiComplianceClient } from '@hhmi/compliance/client';
+import { extension as hhmiPmcClient } from '@hhmi/pmc/client';
+import { extension as opensciDashboardComplianceClient } from '@opensci-dashboard/compliance/client';
+
+export const extensions = [curvenoteScmsSitesExtClient, hhmiComplianceClient, hhmiPmcClient, opensciDashboardComplianceClient];
---
+++ platform/scms/app/extensions/server.ts
@@ -0,0 +1,6 @@
+import { extension as curvenoteScmsSitesExt } from '@curvenote/scms-sites-ext';
+import { extension as hhmiCompliance } from '@hhmi/compliance';
+import { extension as hhmiPmc } from '@hhmi/pmc';
+import { extension as opensciDashboardCompliance } from '@opensci-dashboard/compliance';
+
+export const extensions = [curvenoteScmsSitesExt, hhmiCompliance, hhmiPmc, opensciDashboardCompliance];
