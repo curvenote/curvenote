d69614ed5e3da908c771ca85f544f1fd5e26d746
Adrian Gutierrez <ingenieria.aga@gmail.com>
2026-09-17 13:50:35 +0100

    ✨ Add the DOI prefix normaliser (CN-2508)

---
+++ ee/sites/src/backend/crossref/prefix.spec.ts
@@ -0,0 +1,32 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { describe, expect, test } from 'vitest';
+import { normalizePrefix } from './prefix.js';
+
+describe('normalizePrefix', () => {
+  test.each([
+    ['10.5555', '10.5555'],
+    ['  10.5555  ', '10.5555'],
+    ['10.5555/', '10.5555'],
+    ['doi:10.5555', '10.5555'],
+    ['DOI: 10.5555', '10.5555'],
+    ['https://doi.org/10.5555', '10.5555'],
+    ['https://doi.org/10.5555/', '10.5555'],
+    ['http://dx.doi.org/10.5555', '10.5555'],
+    ['doi.org/10.5555', '10.5555'],
+    ['10.123456789', '10.123456789'],
+  ])('accepts %s', (raw, expected) => {
+    expect(normalizePrefix(raw)).toBe(expected);
+  });
+
+  test.each([
+    [''],
+    ['10.12'],
+    ['11.1234'],
+    ['10.1234/abc'],
+    ['10.1234567890'],
+    ['https://example.org/10.5555'],
+    ['ten.5555'],
+  ])('rejects %s', (raw) => {
+    expect(normalizePrefix(raw)).toBeNull();
+  });
+});
